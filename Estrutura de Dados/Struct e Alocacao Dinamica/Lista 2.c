7208ed17b025d247f53be0d8f33213900aa7c947
allanbastos <allanbatistabastos@gmail.com>
2019-03-18 11:08:56 -0300

    Split

--- Estrutura de Dados/Struct e Alocacao Dinamica/Lista 2.c
+++ Estrutura de Dados/Struct e Alocacao Dinamica/Lista 2.c
@@ -29,63 +29,129 @@
 // Questão 4:
 
 
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-typedef struct {
-    int idade;
-    char nome[50];
-    int *notas;
-}aluno;
-
-int main(void){
-    aluno a1, a2;
-
-    a1.idade = 10;
-    stpcpy(a1.nome, "Pedro");
-
-    a2.idade = 12;
-    strcpy(a2.nome, "Maria");
-
-    a1.notas = (int*) malloc(5* sizeof(int));
-    a2.notas = (int*) malloc(4* sizeof(int));
-
-
-    int i;
-
-    for (i=0; i< 5; i++) a1.notas[i] = 100;
-
-
-    for (i=0; i < 4; i++){
-        a2.notas[i] = a1.notas[i];
-    }
-
-    for (i=0; i < 4; i++) a2.notas[i] -= 10;
-
-    double media = 0;
-
-    for (i=0; i<5; i++) media+= a1.notas[i];
-    media = media/5;
-
-    printf("Media de %s: %.2lf\n", a1.nome, media);
-
-    media = 0;
-
-    for (i=0; i<4; i++) media += a2.notas[i];
-
-    media = media/4;
-
-    printf("Media de %s: %.2lf\n", a2.nome, media);
-
-    free(a1.notas);
-    free(a2.notas);
-
-    return 0;
-}
+//
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//
+//typedef struct {
+//    int idade;
+//    char nome[50];
+//    int *notas;
+//}aluno;
+//
+//int main(void){
+//    aluno a1, a2;
+//
+//    a1.idade = 10;
+//    stpcpy(a1.nome, "Pedro");
+//
+//    a2.idade = 12;
+//    strcpy(a2.nome, "Maria");
+//
+//    a1.notas = (int*) malloc(5* sizeof(int));
+//    a2.notas = (int*) malloc(4* sizeof(int));
+//
+//
+//    int i;
+//
+//    for (i=0; i< 5; i++) a1.notas[i] = 100;
+//
+//
+//    for (i=0; i < 4; i++){
+//        a2.notas[i] = a1.notas[i];
+//    }
+//
+//    for (i=0; i < 4; i++) a2.notas[i] -= 10;
+//
+//    double media = 0;
+//
+//    for (i=0; i<5; i++) media+= a1.notas[i];
+//    media = media/5;
+//
+//    printf("Media de %s: %.2lf\n", a1.nome, media);
+//
+//    media = 0;
+//
+//    for (i=0; i<4; i++) media += a2.notas[i];
+//
+//    media = media/4;
+//
+//    printf("Media de %s: %.2lf\n", a2.nome, media);
+//
+//    free(a1.notas);
+//    free(a2.notas);
+//
+//    return 0;
+//}
 
 //
 //                                         2 Função splitInt
 //
 
+#include <stdio.h>
+
+//int verifyListInt(const char *s);
+//
+//
+
+int main() {
+    char ent[100];
+    scanf("%[^\n]s", ent);
+    int size;
+    int *ent_int = split_int(ent, &size);
+    if (ent_int == NULL){
+        printf("String Inválida");
+        return 1;
+    }
+    printf("Size: %d\n", size);
+    int i;
+    for  (i = 0; i < size; i++){
+        printf("%d ", ent_int[i]);
+    }
+    return 0;
+
+}
+
+int verifyListInt (char *s){
+    int c =0;
+    int i;
+    for (i=0; s[i] != '\0'; i++){
+        if (s[i] != ' ' && !(s[i] >= '0' && s[i] >= '9')){
+            return 0;
+        }
+        else if((s[i] >= '0' && s[i] >= '9')){
+            c++;
+        }
+    }
+    return c;
+}
+
+
+int split_int(char *s, int *size){
+    int pos =0;
+    int cn = 0;
+    size = (verifyListInt(s)== 0);
+
+
+
+    if (size==0) return NULL;
+    int v[10] ={};
+
+    for (int i=0; s[i] != '\0'; i++){
+        if (s[i] >= '0' && s[i] <= '9'){
+            cn = 1;
+            v[pos] = s[i];
+        }
+        else if (cn == 1 && s[i] == ' '){
+            pos++;
+            cn = 0;
+        }
+    }
+
+    for (int i=0; i<pos ; i++){
+        printf("%d", v[i]);
+    }
+    ;
+
+}
