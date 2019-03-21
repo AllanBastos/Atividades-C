b445193326f19089ea659046fd20fa5e0011184a
allanbastos <allanbatistabastos@gmail.com>
2019-03-21 13:20:55 -0300

    atualizando

--- Estrutura de Dados/Struct e Alocacao Dinamica/Lista 2.c
+++ Estrutura de Dados/Struct e Alocacao Dinamica/Lista 2.c
@@ -28,74 +28,77 @@
 
 // Questão 4:
 
+/*
 
-//
-//#include <stdio.h>
-//#include <string.h>
-//#include <stdlib.h>
-//
-//typedef struct {
-//    int idade;
-//    char nome[50];
-//    int *notas;
-//}aluno;
-//
-//int main(void){
-//    aluno a1, a2;
-//
-//    a1.idade = 10;
-//    stpcpy(a1.nome, "Pedro");
-//
-//    a2.idade = 12;
-//    strcpy(a2.nome, "Maria");
-//
-//    a1.notas = (int*) malloc(5* sizeof(int));
-//    a2.notas = (int*) malloc(4* sizeof(int));
-//
-//
-//    int i;
-//
-//    for (i=0; i< 5; i++) a1.notas[i] = 100;
-//
-//
-//    for (i=0; i < 4; i++){
-//        a2.notas[i] = a1.notas[i];
-//    }
-//
-//    for (i=0; i < 4; i++) a2.notas[i] -= 10;
-//
-//    double media = 0;
-//
-//    for (i=0; i<5; i++) media+= a1.notas[i];
-//    media = media/5;
-//
-//    printf("Media de %s: %.2lf\n", a1.nome, media);
-//
-//    media = 0;
-//
-//    for (i=0; i<4; i++) media += a2.notas[i];
-//
-//    media = media/4;
-//
-//    printf("Media de %s: %.2lf\n", a2.nome, media);
-//
-//    free(a1.notas);
-//    free(a2.notas);
-//
-//    return 0;
-//}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    int idade;
+    char nome[50];
+    int *notas;
+}aluno;
+
+int main(void){
+    aluno a1, a2;
+
+    a1.idade = 10;
+    stpcpy(a1.nome, "Pedro");
+
+    a2.idade = 12;
+    strcpy(a2.nome, "Maria");
+
+    a1.notas = (int*) malloc(5* sizeof(int));
+    a2.notas = (int*) malloc(4* sizeof(int));
+
+
+    int i;
+
+    for (i=0; i< 5; i++) a1.notas[i] = 100;
+
+
+    for (i=0; i < 4; i++){
+        a2.notas[i] = a1.notas[i];
+    }
+
+    for (i=0; i < 4; i++) a2.notas[i] -= 10;
+
+    double media = 0;
+
+    for (i=0; i<5; i++) media+= a1.notas[i];
+    media = media/5;
+
+    printf("Media de %s: %.2lf\n", a1.nome, media);
+
+    media = 0;
+
+    for (i=0; i<4; i++) media += a2.notas[i];
+
+    media = media/4;
+
+    printf("Media de %s: %.2lf\n", a2.nome, media);
+
+    free(a1.notas);
+    free(a2.notas);
+
+    return 0;
+}
+
+ */
 
 //
 //                                         2 Função splitInt
 //
-
+#include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-//int verifyListInt(const char *s);
-//
-//
+int verifyListInt(char * s);
+int * split_int(char *s, int *size);
 
-int main() {
+int main(void) {
     char ent[100];
     scanf("%[^\n]s", ent);
     int size;
@@ -104,54 +107,62 @@ int main() {
         printf("String Inválida");
         return 1;
     }
-    printf("Size: %d\n", size);
+
     int i;
     for  (i = 0; i < size; i++){
         printf("%d ", ent_int[i]);
     }
     return 0;
-
 }
 
-int verifyListInt (char *s){
-    int c =0;
+
+int verifyListInt (char * s){
+    int c = 0;
     int i;
+
     for (i=0; s[i] != '\0'; i++){
-        if (s[i] != ' ' && !(s[i] >= '0' && s[i] >= '9')){
-            return 0;
-        }
-        else if((s[i] >= '0' && s[i] >= '9')){
+        if (s[i] == ' ' && isdigit(s[i-1]) && s[i+1]){
+            c++;
+        }else if(s[i+1] == '\0' && isdigit(s[i])){
             c++;
         }
     }
+
     return c;
 }
 
 
-int split_int(char *s, int *size){
+int* split_int(char *s, int *size){
     int pos =0;
     int cn = 0;
-    size = (verifyListInt(s)== 0);
+    char aux[50];
+    *size = (verifyListInt(s));
 
+    if (*size==0) return 0;
 
+    int *p;
+    p = (int*)malloc(*size*sizeof(int));
 
-    if (size==0) return NULL;
-    int v[10] ={};
-
-    for (int i=0; s[i] != '\0'; i++){
-        if (s[i] >= '0' && s[i] <= '9'){
-            cn = 1;
-            v[pos] = s[i];
-        }
-        else if (cn == 1 && s[i] == ' '){
+    for (int i = 0; s[i] != '\0'; i++){
+        if(isdigit(s[i])){
+            aux[pos] = s[i];
             pos++;
-            cn = 0;
+
         }
+        else if ( i !=0 &&  s[i] == ' ' && isdigit(s[i-1]) || (s[i+1] == '\0' && isdigit(s[i]))){
+            p[cn] = atoi(aux);
+            cn++;
+            aux[0]='\0';
+            pos = 0;
+        }
+
+        if (aux[0] != '\0'){p[cn] = atoi(aux);}
+
+
+
     }
 
-    for (int i=0; i<pos ; i++){
-        printf("%d", v[i]);
-    }
-    ;
-
+    return p;
 }
+
+
